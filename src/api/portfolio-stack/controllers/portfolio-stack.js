71889e85f96e73ee12233dd4b16e0892896a07c4
Anwar Ramadha <anwarramadha22@gmail.com>
2024-08-21 00:09:20 +0700

    feat: :sparkles: init

---
+++ src/api/portfolio-stack/controllers/portfolio-stack.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * portfolio-stack controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::portfolio-stack.portfolio-stack');
---
+++ src/api/portfolio-stack/routes/portfolio-stack.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * portfolio-stack router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::portfolio-stack.portfolio-stack');
